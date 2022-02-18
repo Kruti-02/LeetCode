5ef07ecd757f3d586b6c85ca3fc96dc0dbba70b2
Kruti-02 <50515544+Kruti-02@users.noreply.github.com>
2022-02-18 10:52:56 -0500

    Time: 47 ms (70.05%), Space: 14.2 MB (67.26%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -1,18 +1,16 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        numStack = [] 
+        numstack=[]
         
-        # Construct a monotone increasing sequence of digits
-        for digit in num: 
-            while k and numStack and numStack[-1] > digit:
-                numStack.pop()
-                k -= 1 
+        for digit in num:
+            while k and numstack and numstack[-1] > digit:
+                numstack.pop()
+                k -= 1
+                
+            numstack.append(digit)
+                
+        finalstack = numstack[:-k] if k else numstack
         
-            numStack.append(digit)
+        return "".join(finalstack).lstrip('0') or "0"
+                
         
-        # - Trunk the remaining K digits at the end
-        # - in the case k==0: return the entire list
-        finalStack = numStack[:-k] if k else numStack
-        
-        # trip the leading zeros
-        return "".join(finalStack).lstrip('0') or "0"
