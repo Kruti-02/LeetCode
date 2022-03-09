acdd77358ac9c1750b121ecab864e2451cb2bb94
Kruti-02 <50515544+Kruti-02@users.noreply.github.com>
2022-03-09 16:20:50 -0500

    Time: 90 ms (64.86%), Space: 15.2 MB (55.53%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        nums_map = {}
-        for i in range(0,len(nums)):
-            nums_map[nums[i]] = i
+        premap = {}
         
-        for i in range(0, len(nums)):
-            difference = target - nums[i]
-            
-            if difference in nums_map and nums_map[difference] !=i:
-                return [i, nums_map[difference]]
-        return -1
+        for i,n in enumerate(nums):
+            diff = target-n
+            if diff in premap:
+                return [premap[diff],i]
+            premap[n] = i
+        return
+        
+        
             
         
