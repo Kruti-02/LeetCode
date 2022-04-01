c28b4a5c0fccf6a857568ab6ad3ef66b80e9d236
Kruti-02 <50515544+Kruti-02@users.noreply.github.com>
2022-04-01 18:29:17 -0400

    Time: 312 ms (53.19%), Space: 25.4 MB (79.74%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        n = len(nums)
-        a = [0]*n
-        for i in range(n):
+        n = len(nums) 
+        a = [0]*n      
+        for i in range(n): 
             a[(i+k) % n] = nums[i]
             
         nums[:] = a
