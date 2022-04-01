05f562662c3b6f48b1d593eb9fc6fd3314f6888e
Kruti-02 <50515544+Kruti-02@users.noreply.github.com>
2022-04-01 19:14:41 -0400

    Time: 73 ms (40.74%), Space: 14 MB (52.82%) - LeetHub

---
+++ 1165-single-row-keyboard/1165-single-row-keyboard.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calculateTime(self, keyboard: str, word: str) -> int:
+        layout = {}
+        time = 0
+        index = 0
+        for i in range(len(keyboard)):
+            layout[keyboard[i]] = i
+        
+        for c in word:
+            time += abs(index - layout[c])
+            index = layout[c]
+        return time
+    
+    
+        
