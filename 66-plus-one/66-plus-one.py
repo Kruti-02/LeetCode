8281bbf28a79ee5788b5ca91094217c87ebc60c6
Kruti-02 <50515544+Kruti-02@users.noreply.github.com>
2022-05-21 00:45:01 -0400

    Time: 51 ms (33.29%), Space: 13.8 MB (59.53%) - LeetHub

---
+++ 66-plus-one/66-plus-one.py
@@ -0,0 +1,19 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        number = 0
+        multiplier = 10 ** (len(digits) - 1)
+        
+        for digit in digits:
+            number = number + digit * multiplier
+            multiplier = multiplier//10
+        
+        #print(number)
+        
+        number +=1 # 4322
+        
+        result = []
+        while number:
+            result.insert(0, number%10) #[]
+            number = number//10
+        return result
