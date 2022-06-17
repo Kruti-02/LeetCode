ffdf81f2c4c92a9f0583ee8a17e457cc58eb43a8
Kruti-02 <50515544+Kruti-02@users.noreply.github.com>
2022-06-16 23:45:55 -0400

    Time: 89 ms (64.46%), Space: 15.2 MB (24.04%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,18 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #Create a hash_map where key = element of nums and value is the index of that element
         hash_map = {}
-        #Iterating through every value in array
+        
         for i,n in enumerate(nums):
-            #Check the difference
             diff = target - n
-            #Check if difference exist in hashmap
             if diff in hash_map:
-                return [hash_map[diff],i]
-            #If element does't exixst to map add that element
-            hash_map[n] = i
-        return
-        
-        
+                return [hash_map[diff], i]
+            hash_map[n] = i 
             
         
